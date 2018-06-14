aa7f2b11eda8262e8d35e1831a772c8da3ecfe84
Paul Maigutyak <pmaigutyak@ettera.com>
2018-06-14 19:27:16 +0300

    Remove product attr mixin.

--- attributes/models/__init__.py
+++ attributes/models/__init__.py
@@ -1,7 +1,5 @@
 
 from attributes.models.product_attr import ProductAttr
 from attributes.models.product_attr_option import ProductAttrOption
-from attributes.models.product_attr_service import ProductAttrService
 from attributes.models.product_attr_value import ProductAttrValue, VALUE_FIELDS
-from attributes.models.product_attr_mixins import (
-    ProductAttrMixin, ProductCategoryAttrMixin)
+from attributes.models.product_attr_mixins import ProductCategoryAttrMixin
